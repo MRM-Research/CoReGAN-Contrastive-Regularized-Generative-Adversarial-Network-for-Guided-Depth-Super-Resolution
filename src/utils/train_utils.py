3dfa0649323a126dfde443a7c1320a6b5afbaead
IshaanGakhar <106798053+IshaanGakhar@users.noreply.github.com>
2023-09-04 23:14:33 +0530

    bug ficks

--- src/utils/train_utils.py
+++ src/utils/train_utils.py
@@ -119,8 +119,8 @@ class Epoch:
 
     def _to_device(self):
         self.net_g.to(self.device)
-        self.Gloss.to(self.device)
-        self.Gloss.to(self.device)
+        self.GLoss.to(self.device)
+        self.MLoss.to(self.device)
 
     def _format_logs(self, logs):
         str_logs = ["{} - {:.4}".format(k, v) for k, v in logs.items()]
