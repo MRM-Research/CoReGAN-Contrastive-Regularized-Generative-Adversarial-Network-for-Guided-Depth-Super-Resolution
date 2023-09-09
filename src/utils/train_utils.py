4351f880c13400016f22d63be84a48c364e093fa
IshaanGakhar <106798053+IshaanGakhar@users.noreply.github.com>
2023-09-09 18:35:09 +0530

    Removing unncessary code in ValidEpoch

--- src/utils/train_utils.py
+++ src/utils/train_utils.py
@@ -201,7 +201,7 @@ class TrainEpoch(Epoch):
     
     def batch_update(self, current_iter , rgb, depth_high_res, depth_low_res):
         
-        self.rgb,self.depth_high_res,self.depth_low_res = rgb, depth_high_res, depth_low_res
+        self.rgb, self.depth_high_res, self.depth_low_res = rgb, depth_high_res, depth_low_res
        
         # creating a list of optimizers to allow integration of lr_schedul
         self.optimizers = [self.optimizer_g, self.optimizer_d]
@@ -308,12 +308,21 @@ class ValidEpoch(Epoch):
         self.contrastive = contrastive
         self.schedulers = []
 
+    def get_current_visuals(self):
+        out_dict = OrderedDict()
+        out_dict['depth_low_res'] = self.depth_low_res.detach().to(self.device)
+        out_dict['result'] = self.output.detach().to(self.device)
+        out_dict['rgb'] = self.rgb.detach().to(self.device)
+        if hasattr(self, 'depth_high_res'):
+            out_dict['depth_high_res'] = self.depth_high_res.detach().to(self.device)
+        return out_dict
+
     def on_epoch_start(self):
         self.net_g.eval()
 
     def batch_update(self, current_iter, rgb, depth_high_res, depth_low_res):
 
-        self.rgb,self.depth_high_res,self.depth_low_res = rgb, depth_high_res, depth_low_res
+        self.rgb, self.depth_high_res, self.depth_low_res = rgb, depth_high_res, depth_low_res
         
         self.net_d_iters = 1
         self.net_d_init_iters = 0
@@ -326,7 +335,7 @@ class ValidEpoch(Epoch):
 
         # initiliazing l_g_total to 0
         l_g_total = 0
-        loss_dict = OrderedDict()           
+        loss_dict = OrderedDict()
 
         if (current_iter % self.net_d_iters == 0 and current_iter > self.net_d_init_iters):
             
@@ -346,24 +355,8 @@ class ValidEpoch(Epoch):
             p.requires_grad = True
 
         # generating output - not using f1, f2 for valid epoch since contrastive loss is not used
-        self.output, f1, f2 = self.net_g(self.rgb, self.depth_low_res)
-        
-        # real image generation
-        real_d_pred = self.net_d(self.depth_high_res)
-        l_d_real = self.GLoss(real_d_pred, True, is_disc=True).to(self.device)
-        loss_dict['l_d_real'] = l_d_real
-        loss_dict['out_d_real'] = torch.mean(real_d_pred.detach())
-
-        # fake image generation
-        fake_d_pred = self.net_d(self.output)
-        l_d_fake = self.GLoss(fake_d_pred, False, is_disc=True).to(self.device)
-        loss_dict['l_d_fake'] = l_d_fake
-        loss_dict['out_d_fake'] = torch.mean(fake_d_pred.detach())
-
-        # gradient penalty for discriminator
-        self.gp_weight = 100
-        gradient_penalty = compute_gradient_penalty(self.net_d, self.depth_high_res, self.output, self.device)
-        l_d = l_d_real + l_d_fake + (self.gp_weight * gradient_penalty)
+        with torch.no_grad():
+            self.output, f1, f2 = self.net_g(self.rgb, self.depth_low_res)
         
         visuals = self.get_current_visuals()
         guiding_img = visuals['rgb'] 
